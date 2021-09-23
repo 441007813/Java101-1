547f69d29737059a5e99f8627618f97bc8576b2a
mnsornajaa <91257392+mnsornajaa@users.noreply.github.com>
2021-09-23 17:15:10 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
-/*
-Ask the user to enter two float digits. For example ( 3.5 and 11.25 )
 
-Print the calculation for the two digits in separate lines:
-
-Addition +
-Subtraction  -
-Multiplication  *
-Division /
-Modulus %
-Grater >
-Grater or equal
-Less 
-Less or equal
-Not equal
-Equal
-*/
-
-//Your Name
-//Your ID
+//mnsor najaa
+//441007813
 class Main {
   public static void main(String[] args) {
       //Start your code 
-      
+      Scanner myscanner = new Scanner(System.in);
+      float var1 = myscanner.nextFloat();
+      float var2 = myscanner.nextFloat();
+      System.out.println("addition");
+      System.out.print(var1+var2);
+      System.out.println("subtraction");
+      System.out.println(var1 - var2);
+      System.out.println("muitplication");
+      System.out.println(var1 * var2);
+      System.out.println(var1 / var2);
+      System.out.println("moduius");
+      System.out.println(var1 % var2);
+      System.out.println("grater");
+      System.out.println(var1 > var2);
+      System.out.println("grater or equal");
+      System.out.println(var1 >= var2);
+      System.out.println("not equal");
+      System.out.println(var1 != var2);
+      System.out.println("equal");
+      System.out.println(var1 == var2);
+    
   }
 }
